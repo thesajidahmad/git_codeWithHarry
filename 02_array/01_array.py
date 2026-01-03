dd475f8a8366b3413704d0be7ccdc6e5c8f5955d
thesajidahmad <101032376+thesajidahmad@users.noreply.github.com>
2026-01-04 01:28:45 +0530

    create array using 'from array import *'

--- 02_array/01_array.py
+++ 02_array/01_array.py
@@ -1,12 +1,16 @@
 # import array
-import array as arr
+# import array as arr
+from array import *
 
 
 #1 import array   
 # var = array.array('i', [1,2,3,4,5,6,7,8,9]) # here typecode is 'i' that is for int, 'd' for double
 
 #2 import array as arr  
-var = arr.array('i', [1,2,3,4,5,6,7,8,9]) # here typecode is 'i' that is for int, 'd' for double
+# var = arr.array('i', [1,2,3,4,5,6,7,8,9]) # here typecode is 'i' that is for int, 'd' for double
+
+#3 from array import *
+var = array('i', [1,2,3,4,5,6,7,8,9]) # here typecode is 'i' that is for int, 'd' for 
 
 print(f"typecode of var is: {var.typecode}")
 
