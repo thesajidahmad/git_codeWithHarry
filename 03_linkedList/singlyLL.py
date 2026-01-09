f7173bcc1ddc3f6cb40d927f6cd5d26cf374682f
thesajidahmad <101032376+thesajidahmad@users.noreply.github.com>
2026-01-10 03:14:23 +0530

    Add delete function

--- 03_linkedList/singlyLL.py
+++ 03_linkedList/singlyLL.py
@@ -9,9 +9,9 @@ class SinglyLinkedList:
 
     def append(self, value):
         new_node = Node(value)
+
         if(self.head == None):
             self.head = new_node
-        
         else:
             curr_node = self.head
             while(curr_node.next != None):
@@ -20,23 +20,25 @@ class SinglyLinkedList:
 
     def traversal(self):
         if(self.head == None):
-            print("Linked list is empty")
+            print("Linked List is Empty :(")
         else:
             curr_node = self.head
             while(curr_node != None):
                 print(curr_node.data, end=" ")
                 curr_node = curr_node.next
+            print()
 
     def insertAt(self, value, position):
         new_node = Node(value)
-        if (position == 0):
+        curr_node = self.head
+        prev_node = None
+
+        if(position == 0):
             new_node.next = self.head
             self.head = new_node
 
         else:
             count = 0
-            curr_node = self.head
-            prev_node = None
             while(curr_node != None and count < position):
                 prev_node = curr_node
                 curr_node = curr_node.next
@@ -44,7 +46,25 @@ class SinglyLinkedList:
             prev_node.next = new_node
             new_node.next = curr_node
 
+    def delete(self, value):
+        curr_node = self.head
+        if(curr_node.next != None):
+            if(curr_node.data == value):
+                self.head = curr_node.next
+                return
+            else:
+                found = False
+                prev_node = None
+                while(curr_node != None):
+                    if (curr_node.value == value):
+                        found = True
+                    prev_node = curr_node
+                    curr_node = curr_node.next
+                if found:
+                    prev_node.next = curr_node.next
 
+                else:
+                    print("Node not Found :(")
 
 
 sll = SinglyLinkedList()
@@ -52,5 +72,9 @@ sll.append(10)
 sll.append(20)
 sll.append(30)
 sll.append(40)
-sll.insertAt(5,2)
+
+sll.insertAt(22,2)
+sll.traversal()
+
+sll.delete(10)
 sll.traversal()
